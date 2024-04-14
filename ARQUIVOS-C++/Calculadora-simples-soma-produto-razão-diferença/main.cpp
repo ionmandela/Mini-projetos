7e21ed2555b8f86ed95bc73b4a91163bf7aea985
Ion Mandela <ionmandela@gmail.com>
2024-04-14 16:44:45 -0300

    ajustes

--- ARQUIVOS-C++/Calculadora-simples-soma-produto-razão-diferença/main.cpp
+++ ARQUIVOS-C++/Calculadora-simples-soma-produto-razão-diferença/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <locale>
 using namespace std;
 
 int main(){
+
     setlocale(LC_ALL, "portuguese");
+
     cout << "=============================================================" << endl;
-    cout << "CALCULADORA - SOMA, PRODUTO, RAZAO E "
+    cout << "CALCULADORA - SOMA, DIFERENÇA, PRODUTO E RAZÃO ";
     
     return 0;
 }
