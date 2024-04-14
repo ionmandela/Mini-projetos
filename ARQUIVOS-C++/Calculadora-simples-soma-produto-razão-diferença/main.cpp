e4660dc69576b8aa0545bff2c4475e0978d7fb90
Ion Mandela <ionmandela@gmail.com>
2024-04-14 16:50:02 -0300

    consertando acentuação

--- ARQUIVOS-C++/Calculadora-simples-soma-produto-razão-diferença/main.cpp
+++ ARQUIVOS-C++/Calculadora-simples-soma-produto-razão-diferença/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <locale>
+#include <windows.h>
 using namespace std;
 
 int main(){
-
-    setlocale(LC_ALL, "portuguese");
+    SetConsoleOutputCP(CP_UTF8);
 
     cout << "=============================================================" << endl;
     cout << "CALCULADORA - SOMA, DIFERENÇA, PRODUTO E RAZÃO ";
-    
+
     return 0;
 }
